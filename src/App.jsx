90da26ac404ef3aeeacb9664907e4930c4c35b65
freetimezp <kraftukrnet@ukr.net>
2024-07-14 18:45:17 +0300

    08. js-portfolio-102-realist. Agents component.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Clients from "./components/Clients/Clients";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
@@ -17,8 +20,8 @@ function App() {
       <About />
       <Services />
       <Properties />
-      {/* <Agents />
-      <Clients />
+      <Agents />
+      {/* <Clients />
       <Contact />
       <Footer /> */}
     </>
--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -1,11 +1,75 @@
+import React, { useRef } from "react";
 import './Agents.css';
 
+import Slider from "react-slick";
+
+import { agents } from "../../sources";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+
 const Agents = () => {
+    const ref = useRef(null);
+
+    const settings = {
+        Infinity: true,
+        speed: 500,
+        slidesToShow: 3,
+        centerMode: true,
+        pauseOnHover: true,
+        autoplay: true,
+        autoplayspeed: 3000,
+        centerPadding: 0,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    };
+
     return (
-        <div>
-            Agents
-        </div>
-    )
+        <section id="agents">
+            <div className="wrapper">
+                <h1 className="heading-1">
+                    <span className="gradient-text">Agents</span>
+                </h1>
+
+                <Slider {...settings} className='agent__container' ref={ref}>
+                    {agents.map((agent, index) => (
+                        <React.Fragment key={index}>
+                            <div className="profile">
+                                <img src={agent.profile} alt="agent" />
+                            </div>
+                            <h3 className="name">{agent.name}</h3>
+                            <div className="flex-center social-container">
+                                {agent.socialHandles.map((list, i) => (
+                                    <a key={i} href={list.url} className="flex-center icon-wrapper">
+                                        {list.icon}
+                                    </a>
+                                ))}
+                            </div>
+                        </React.Fragment>
+                    ))}
+                </Slider>
+
+                <div className="flex-center buttons-container">
+                    <button className="btn flex-center" onClick={() => ref.current.slickPrev()}>
+                        <FaLongArrowAltLeft />
+                    </button>
+                    <button className="btn flex-center" onClick={() => ref.current.slickNext()}>
+                        <FaLongArrowAltRight />
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
 }
 
-export default Agents
+export default Agents;
