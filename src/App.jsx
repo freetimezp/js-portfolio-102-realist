4c3425d61e96f5a2366924bd4ae30c35f0ccb317
freetimezp <kraftukrnet@ukr.net>
2024-07-19 22:41:45 +0300

    11. js-portfolio-102-realist. Footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <Agents />
       <Clients />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,57 @@
 import './Footer.css';
 
+import { contactOptions, footer } from '../../sources';
+
+import footerImage from "../../assets/realist-footer.png";
+import Logo from '../Logo/Logo';
+import Socials from '../Socials/Socials';
+import { Link } from 'react-scroll';
+
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer id="footer">
+            <div className="wrapper">
+                <div className="contact-options">
+                    {contactOptions.map((option, index) => (
+                        <div key={index} className='flex-center option'>
+                            <div className="flex-center icon-wrapper">{option.icon}</div>
+                            <h4 className="muted title">{option.title}</h4>
+                            <h3 className="value">{option.value}</h3>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="bottom-row">
+                <div className="wrapper">
+                    <div className="column">
+                        <Logo />
+
+                        <div className="footer-image">
+                            <img src={footerImage} alt="footer" />
+                        </div>
+
+                        <Socials />
+                    </div>
+
+                    {footer.map((list, index) => (
+                        <div className="column" key={index}>
+                            <h3 className="muted title">{list.title}</h3>
+                            {list.routes.map((route, i) => (
+                                <Link key={i} to={route.id || ""} smooth={true} className='route'>
+                                    {route.name}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+
+                <div className="flex-center copyright">
+                    <h4>&copy; Freetime 2024. All Rights Reserved.</h4>
+                    <p className="muted">Have a nice day!</p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
