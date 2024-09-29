ceb8c60768a8796808a10c0c3c1e19747d98cfe7
freetimezp <kraftukrnet@ukr.net>
2024-09-29 15:41:25 +0300

    13. js-portfolio-102-realist. css.

--- src/sources.jsx
+++ src/sources.jsx
@@ -12,168 +12,168 @@ import agent3 from "./assets/agent-3.jpeg";
 import agent4 from "./assets/agent-4.jpeg";
 import agent5 from "./assets/agent-5.jpeg";
 import agent6 from "./assets/agent-6.jpeg";
-import { IoMdAnalytics} from "react-icons/io";
-import { IoCallOutline ,IoLocationOutline } from "react-icons/io5";
-import { GrUserExpert,GrUserManager } from "react-icons/gr";
-import { MdOutlineSupportAgent,MdOutlineAlternateEmail } from "react-icons/md";
+import { IoMdAnalytics } from "react-icons/io";
+import { IoCallOutline, IoLocationOutline } from "react-icons/io5";
+import { GrUserExpert, GrUserManager } from "react-icons/gr";
+import { MdOutlineSupportAgent, MdOutlineAlternateEmail } from "react-icons/md";
 import { RiExchange2Fill } from "react-icons/ri";
-import { CiFacebook,CiLinkedin } from "react-icons/ci";
-import { FaInstagram, FaXTwitter, FaYoutube,FaDrawPolygon} from "react-icons/fa6";
+import { CiFacebook, CiLinkedin } from "react-icons/ci";
+import { FaInstagram, FaXTwitter, FaYoutube, FaDrawPolygon } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import { SiBlueprint } from "react-icons/si";
 
 
 export const tabs = [
-    {name:"Home",id:'hero'},
-    {name:"About Us",id:'about'},
-    {name:"Services",id:'services'},
-    {name:"Properties",id:'properties'},
-    {name:"Agents",id:'agents'}
+  { name: "Home", id: 'hero' },
+  { name: "About Us", id: 'about' },
+  { name: "Services", id: 'services' },
+  { name: "Properties", id: 'properties' },
+  { name: "Agents", id: 'agents' }
 ]
 
 export const whyChooseUs = [
   {
-    title:"Unmatched Local Expertise",
-    icon:<GrUserExpert/>,
-    link:"",
+    title: "Unmatched Local Expertise",
+    icon: <GrUserExpert />,
+    link: "",
   },
   {
-    title:"Comprehensive Market Analysise",
-    icon:<IoMdAnalytics/>,
-    link:"",
+    title: "Comprehensive Market Analysise",
+    icon: <IoMdAnalytics />,
+    link: "",
   },
   {
-    title:"Dedicated Client Support",
-    icon:<MdOutlineSupportAgent/>,
-    link:"",
+    title: "Dedicated Client Support",
+    icon: <MdOutlineSupportAgent />,
+    link: "",
   },
   {
-    title:"Innovative Marketing Strategies",
-    icon:<RiExchange2Fill/>,
-    link:"",
+    title: "Innovative Marketing Strategies",
+    icon: <RiExchange2Fill />,
+    link: "",
   },
 ]
 export const services = [
   {
-    name:"General Contracting",
-    icon:<SiBlueprint/>,
-    description:`This involves overseeing the entire construction project from start to finish, 
+    name: "General Contracting",
+    icon: <SiBlueprint />,
+    description: `This involves overseeing the entire construction project from start to finish, 
         including hiring subcontractors, managing the budget, scheduling, and ensuring that the
         project meets all specifications and regulations.`,
   },
   {
-    name:"Design-Build Services",
-    icon:<FaDrawPolygon/>,
-    description:`This service combines both the design and construction phases into a single 
+    name: "Design-Build Services",
+    icon: <FaDrawPolygon />,
+    description: `This service combines both the design and construction phases into a single 
        contract, streamlining the process and improving communication between the design and
         construction teams.
 
     `,
   },
   {
-    name:"Project Management",
-    icon:<GrUserManager/>,
-    description:`Construction companies often provide project management services, which include 
+    name: "Project Management",
+    icon: <GrUserManager />,
+    description: `Construction companies often provide project management services, which include 
        planning, coordinating, and overseeing projects to ensure they are completed on time, within 
        budget, and to the required quality standards.`,
   },
-  
+
 ]
 
 
 export const properties = [
   {
-    title:"Spectacular Oceanfront Villa with Private Beach Access",
-    image:building1,
-    description:` Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    title: "Spectacular Oceanfront Villa with Private Beach Access",
+    image: building1,
+    description: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Animi atque fugiat doloremque similique, magnam`,
-    price:"$35,000",
-    bedrooms:"2",
-    bathrooms:"3",
-    location:"Abuja, Nigeria",
+    price: "$35,000",
+    bedrooms: "2",
+    bathrooms: "3",
+    location: "Odessa, Ukraine",
   },
   {
-    title:"Chic Downtown Loft with Panoramic City Views",
-    image:building2,
-    description:` Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    title: "Chic Downtown Loft with Panoramic City Views",
+    image: building2,
+    description: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. 
              Animi atque fugiat doloremque similique, magnam`,
-    price:"$90,500",
-    bedrooms:"4",
-    bathrooms:"6",
-    location:"Lagos, Nigeria",
+    price: "$90,500",
+    bedrooms: "4",
+    bathrooms: "6",
+    location: "Kyiv, Ukraine",
   },
   {
-    title:"Majestic Napa Valley Estate with Vineyard and Winery",
-    image:building3,
-    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    title: "Majestic Estate",
+    image: building3,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Animi atque fugiat doloremque similique, magnam`,
-    price:"$82,000",
-    bedrooms:"3",
-    bathrooms:"4",
-    location:"Enugu, Nigeria",
+    price: "$82,000",
+    bedrooms: "3",
+    bathrooms: "4",
+    location: "Lviv, Ukraine",
   },
 ]
 
 
 export const agents = [
   {
-    name:"Mary Arum",
-    profile:agent1,
-    socialHandles:[
-      {name:"Facebook",icon:<CiFacebook/>,url:"http://facebook.com"},
-      {name:"LinkedIn",icon:<CiLinkedin/>,url:"http://linkedin.com"},
-      {name:"Youtube",icon:<FaYoutube/>,url:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1"},
-      {name:"Instagram",icon:<FaInstagram/>,url:"http://instagram.com"},
+    name: "Mary Arum",
+    profile: agent1,
+    socialHandles: [
+      { name: "Facebook", icon: <CiFacebook />, url: "http://facebook.com" },
+      { name: "LinkedIn", icon: <CiLinkedin />, url: "http://linkedin.com" },
+      { name: "Youtube", icon: <FaYoutube />, url: "https://www.youtube.com/" },
+      { name: "Instagram", icon: <FaInstagram />, url: "http://instagram.com" },
     ]
   },
   {
-    name:"Kelvin Dan",
-    profile:agent2,
-    socialHandles:[
-      {name:"Facebook",icon:<CiFacebook/>,url:"http://facebook.com"},
-      {name:"LinkedIn",icon:<CiLinkedin/>,url:"http://linkedin.com"},
-      {name:"Youtube",icon:<FaYoutube/>,url:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1"},
-      {name:"Instagram",icon:<FaInstagram/>,url:"http://instagram.com"},
+    name: "Kelvin Dan",
+    profile: agent2,
+    socialHandles: [
+      { name: "Facebook", icon: <CiFacebook />, url: "http://facebook.com" },
+      { name: "LinkedIn", icon: <CiLinkedin />, url: "http://linkedin.com" },
+      { name: "Youtube", icon: <FaYoutube />, url: "https://www.youtube.com/" },
+      { name: "Instagram", icon: <FaInstagram />, url: "http://instagram.com" },
     ]
   },
   {
-    name:"Joy Eze",
-    profile:agent3,
-    socialHandles:[
-      {name:"Facebook",icon:<CiFacebook/>,url:"http://facebook.com"},
-      {name:"LinkedIn",icon:<CiLinkedin/>,url:"http://linkedin.com"},
-      {name:"Youtube",icon:<FaYoutube/>,url:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1"},
-      {name:"Instagram",icon:<FaInstagram/>,url:"http://instagram.com"},
+    name: "Joy Eze",
+    profile: agent3,
+    socialHandles: [
+      { name: "Facebook", icon: <CiFacebook />, url: "http://facebook.com" },
+      { name: "LinkedIn", icon: <CiLinkedin />, url: "http://linkedin.com" },
+      { name: "Youtube", icon: <FaYoutube />, url: "https://www.youtube.com/" },
+      { name: "Instagram", icon: <FaInstagram />, url: "http://instagram.com" },
     ]
   },
   {
-    name:"Sunday Oga",
-    profile:agent4,
-    socialHandles:[
-      {name:"Facebook",icon:<CiFacebook/>,url:"http://facebook.com"},
-      {name:"LinkedIn",icon:<CiLinkedin/>,url:"http://linkedin.com"},
-      {name:"Youtube",icon:<FaYoutube/>,url:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1"},
-      {name:"Instagram",icon:<FaInstagram/>,url:"http://instagram.com"},
+    name: "Sunday Oga",
+    profile: agent4,
+    socialHandles: [
+      { name: "Facebook", icon: <CiFacebook />, url: "http://facebook.com" },
+      { name: "LinkedIn", icon: <CiLinkedin />, url: "http://linkedin.com" },
+      { name: "Youtube", icon: <FaYoutube />, url: "https://www.youtube.com/" },
+      { name: "Instagram", icon: <FaInstagram />, url: "http://instagram.com" },
     ]
   },
   {
-    name:"Chisom Precious",
-    profile:agent5,
-    socialHandles:[
-      {name:"Facebook",icon:<CiFacebook/>,url:"http://facebook.com"},
-      {name:"LinkedIn",icon:<CiLinkedin/>,url:"http://linkedin.com"},
-      {name:"Youtube",icon:<FaYoutube/>,url:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1"},
-      {name:"Instagram",icon:<FaInstagram/>,url:"http://instagram.com"},
+    name: "Chisom Precious",
+    profile: agent5,
+    socialHandles: [
+      { name: "Facebook", icon: <CiFacebook />, url: "http://facebook.com" },
+      { name: "LinkedIn", icon: <CiLinkedin />, url: "http://linkedin.com" },
+      { name: "Youtube", icon: <FaYoutube />, url: "https://www.youtube.com/" },
+      { name: "Instagram", icon: <FaInstagram />, url: "http://instagram.com" },
     ]
   },
   {
-    name:"Jennifer Williams",
-    profile:agent6,
-    socialHandles:[
-      {name:"Facebook",icon:<CiFacebook/>,url:"http://facebook.com"},
-      {name:"LinkedIn",icon:<CiLinkedin/>,url:"http://linkedin.com"},
-      {name:"Youtube",icon:<FaYoutube/>,url:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1"},
-      {name:"Instagram",icon:<FaInstagram/>,url:"http://instagram.com"},
+    name: "Jennifer Williams",
+    profile: agent6,
+    socialHandles: [
+      { name: "Facebook", icon: <CiFacebook />, url: "http://facebook.com" },
+      { name: "LinkedIn", icon: <CiLinkedin />, url: "http://linkedin.com" },
+      { name: "Youtube", icon: <FaYoutube />, url: "https://www.youtube.com/" },
+      { name: "Instagram", icon: <FaInstagram />, url: "http://instagram.com" },
     ]
   },
 ]
@@ -181,41 +181,41 @@ export const agents = [
 
 export const clients = [
   {
-    image:client1,
-    name:'Samuel Eze',
-    review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    image: client1,
+    name: 'Some Eze',
+    review: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
            veniam, quis nostrud exercitation ullamco laborisveniam, quis nostrud 
            exercitation ullamco laboris`
   },
   {
-    image:client2,
-    name:'Richael Linda',
-    review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    image: client2,
+    name: 'Richael Linda',
+    review: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
   },
   {
-    image:client3,
-    name:'Gloria Chiwendu',
-    review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    image: client3,
+    name: 'Gloria Chiwendu',
+    review: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
            veniam, quis nostrud exercitation ullamco laborisveniam, quis nostrud 
            exercitation ullamco laborisveniam, quis nostrud exercitation ullamco laboris`
   },
   {
-    image:client4,
-    name:'Precious Stone',
-    review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    image: client4,
+    name: 'Precious Stone',
+    review: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
   },
   {
-    image:client5,
-    name:'Ndubisi John',
-    review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    image: client5,
+    name: 'Ndubisi John',
+    review: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
   },
@@ -224,70 +224,69 @@ export const clients = [
 
 export const contactOptions = [
   {
-    title:"Email",
-    value:"mradinuba@gmail.com",
-    icon:<MdOutlineAlternateEmail />,
+    title: "Email",
+    value: "email@gmail.com",
+    icon: <MdOutlineAlternateEmail />,
   },
   {
-    title:"Phone Number",
-    value:"+2348149121918",
-    icon:<IoCallOutline/>,
+    title: "Phone Number",
+    value: "+0123456789",
+    icon: <IoCallOutline />,
   },
   {
-    title:"Address",
-    value:"Gwarinpa, Abuja, Nigeria",
-    icon:<IoLocationOutline/>,
+    title: "Address",
+    value: "New York, USA",
+    icon: <IoLocationOutline />,
   },
 ]
 
 export const socialHandles = [
-    {
-      name:"Instagram",
-      icon:<FaInstagram />,
-      link:"",
-    },
-    {
-      name:"Facebook",
-      icon:<FaFacebookSquare/>,
-      link:"",
-    },
-    {
-      name:"Twitter",
-      icon:<FaXTwitter/>,
-      link:"",
-    },
-    {
-      name:"Youtube",
-      icon:<FaYoutube/>,
-      link:"https://www.youtube.com/channel/UCEtnsPZQEd0l1tbr_nDQd5Q?sub_confirmation=1",
-    },
+  {
+    name: "Instagram",
+    icon: <FaInstagram />,
+    link: "",
+  },
+  {
+    name: "Facebook",
+    icon: <FaFacebookSquare />,
+    link: "",
+  },
+  {
+    name: "Twitter",
+    icon: <FaXTwitter />,
+    link: "",
+  },
+  {
+    name: "Youtube",
+    icon: <FaYoutube />,
+    link: "https://www.youtube.com/",
+  },
 ];
 
 export const footer = [
   {
-    title:"Explore",
-    routes:[
-      {name:"Home",id:'hero'},
-      {name:"About Us",id:'about'},
-      {name:"Services",id:'services'},
+    title: "Explore",
+    routes: [
+      { name: "Home", id: 'hero' },
+      { name: "About Us", id: 'about' },
+      { name: "Services", id: 'services' },
     ]
   },
   {
-      title:"Properties",
-      routes:[
-        {name:"Properties",id:'properties'},
-        {name:"Agents",id:'agents'},
-        {name:"Contact Us",id:'contact'},
-      ]
-    },
+    title: "Properties",
+    routes: [
+      { name: "Properties", id: 'properties' },
+      { name: "Agents", id: 'agents' },
+      { name: "Contact Us", id: 'contact' },
+    ]
+  },
   {
-      title:"Quick Links",
-      routes:[
-        {name:"Privacy Policy"},
-        {name:"Terms and Conditions"},
-        {name:"Cookie Policy"},
-      ]
-    },
+    title: "Quick Links",
+    routes: [
+      { name: "Privacy Policy" },
+      { name: "Terms and Conditions" },
+      { name: "Cookie Policy" },
+    ]
+  },
 ]
 
-    
